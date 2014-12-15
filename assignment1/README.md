2ff852bbb5b2ef39df230ad43e8973fe171d9eb9
deepak.webonise <deepak.kabbur@weboniselab.com>
2014-12-15 10:03:25 +0530

    Simple Hello world program and iterators

---
+++ assignment1/README.md
@@ -0,0 +1,9 @@
+ small "Hello World" program in Ruby.
+
+Explain how Ruby’s YARV engine executes your code with the following steps - 
+- how will it tokenize?
+- how it will parse and convert your parsed program into AST nodes?
+- what are ruby's grammar rules?
+- how it will compile this parsed code?
+- how will your compiled code look?
+how_YARV_executes_program.odt file contains the explination about RUBY YARV engine execution process.
---
+++ assignment1/hello_world.rb
@@ -0,0 +1,31 @@
+#/usr/bin/ruby -w
+#small "Hello World" program and simple iterators
+
+puts "Hello World"
+
+def multiplier num
+	return Proc.new {|n| puts "#{num} x #{n} = #{num * n}"}
+end
+
+#Prints even numbers
+puts "<<<<Even Numbers>>>>"
+10.times {
+	|n| puts n if n%2 == 0
+}
+
+#Prints Multiplier of user input
+begin
+	puts "Enter Number"
+	user_input = gets
+	n = multiplier Integer(user_input)
+	puts "<<<<multipliers>>>>"
+	(1..10).each{
+		|mul| n.call(mul)
+	}
+rescue Exception => e
+	puts "Invalid Number. Please enter valid number"
+	retry
+end
+
+
+
