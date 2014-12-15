d1ee21024eb0fe7563dfb4d15c558ea69708e961
deepak.webonise <deepak.kabbur@weboniselab.com>
2014-12-15 14:03:42 +0530

    Simple Hello world program and iterators

--- assignment1/hello_world.rb
+++ assignment1/hello_world.rb
@@ -4,25 +4,25 @@
 puts "Hello World"
 
 def multiplier num
-    return Proc.new {|n| puts "#{num} x #{n} = #{num * n}"}
+  return Proc.new {|n| puts "#{num} x #{n} = #{num * n}"}
 end
 
 #Prints even numbers
 puts "<<<<Even Numbers>>>>"
 10.times {
-    |n| puts n if n%2 == 0
+  |i| puts i if i%2 == 0
 }
 
 #Prints Multiplier of user input
 begin
-    puts "Enter Number"
-    user_input = gets
-    number = multiplier Integer(user_input)
-    puts "<<<<multipliers>>>>"
-    (1..10).each{
-        |mul| number.call(mul)
-    }
+  puts "Enter Number"
+  user_input = gets
+  number = multiplier Integer(user_input)
+  puts "<<<<multipliers>>>>"
+  (1..10).each{
+    |i| number.call(i)
+  }
 rescue Exception => e
-    puts "Invalid Number. Please enter valid number"
-    retry
+  puts "Invalid Number. Please enter valid number"
+  retry
 end
