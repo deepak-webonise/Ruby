987008059ac68a68f22a9a71a3fa5dcb46975a72
deepak.webonise <deepak.kabbur@weboniselab.com>
2014-12-22 19:14:42 +0530

    Optimized Regex and case insensitive

--- Regex/regex.rb
+++ Regex/regex.rb
@@ -1,12 +1,16 @@
 #/usr/bin/ruby -w
-#Html scraping
+#Open html file in read mode
 file_html = File.open("states.html", "r")
+
+#Open csv file in write mode
 file_csv = File.open("states.csv", "w")
 file_csv_test = File.open("states_test.csv", "w")
 html_content = file_html.readlines
 html_content.each{
-  |line| match = (line).scan(/(?<=<(?i:t).>)([\w\s]+)(?=<\/t.>)/i)
+  |line| match = (line).scan(/(?<=<t.>)([\w\s]+)(?=<\/t.>)/i)
   if(match.length > 0)
    file_csv.puts(match.join(","))
   end
 }
+file_html.close
+file_csv.close
