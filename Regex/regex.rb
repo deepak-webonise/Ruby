f4507599be7e01245b5e65cefb8ffb2baf6bcf1c
deepak.webonise <deepak.kabbur@weboniselab.com>
2014-12-22 16:12:49 +0530

    Metaprogramming: Parse csv file to create class dynamically

--- Regex/regex.rb
+++ Regex/regex.rb
@@ -2,11 +2,12 @@
 #Html scraping
 file_html = File.open("states.html", "r")
 file_csv = File.open("states.csv", "w")
+file_csv_test = File.open("states_test.csv", "w")
 html_content = file_html.readlines
 html_content.each{
   |line| match = (line).scan(/(?<=<th>)(?<word>(\w)*(\s\w+)*)(?=<\/th>)/)
   if(match.length > 0)
-   file_csv.puts(match.join(";"))
+   file_csv.puts(match.join(":"))
   end
   match = (line).scan(/(?<=<td>)(?<word>(\w)*(\s\w+)*)(?=<\/td>)/)
   if(match.length > 0)
@@ -14,3 +15,17 @@ html_content.each{
   end  
 }
 
+#Testing the unformatted data
+
+#(<t.>)((<)*t.>\w+(\s\w+)*<\/t.>)(<t.>(\w+)*<\/t.>)(<t.>(\w+)*<\/t.>)(<\/t.>)
+html_content = file_html.readlines
+html_content.each{
+  |line| match = (line).scan(/<t?>/)
+  if(match.length > 0)
+   file_csv_test.puts(match.join(";"))
+  end
+  match = (line).scan(/(?<=<td>)(?<word>(\w)*(\s\w+)*)(?=<\/td>)/)
+  if(match.length > 0)
+   file_csv.puts(match.join(";"))
+  end  
+}
---
+++ meta_programming/metaprogram.rb
@@ -0,0 +1,25 @@
+#/usr/bin/ruby -w
+require "csv.rb"
+#CSV file path 
+file_path = "states.csv"
+
+#csv file parsing options
+csv_options = {:headers => true,:col_sep => ";",:header_converters => :symbol,:converters => :all}
+
+#CSV file reading and storing in csv_content
+csv_content = CSV.read(file_path, csv_options)
+
+#Extracting class name from file
+class_name = File.basename(file_path,"s.csv").capitalize
+
+#setting class name as constant
+Object.const_set(class_name,Class.new)
+class_csv=  Object.const_get(class_name)
+csv_content.each do  |row|
+  obj_csv = class_csv.new
+  row.each do |instance_var, instance_val|
+    class_csv.class_eval{attr_accessor instance_var}
+    obj_csv.instance_variable_set("@#{instance_var}",instance_val)
+  end
+  puts obj_csv.inspect
+end
