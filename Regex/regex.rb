0cecaf9d796b658c2cc114f99c1edcdc161aef17
deepak.webonise <deepak.kabbur@weboniselab.com>
2014-12-22 17:35:50 +0530

    Regex optimized

--- Regex/regex.rb
+++ Regex/regex.rb
@@ -5,27 +5,8 @@ file_csv = File.open("states.csv", "w")
 file_csv_test = File.open("states_test.csv", "w")
 html_content = file_html.readlines
 html_content.each{
-  |line| match = (line).scan(/(?<=<th>)(?<word>(\w)*(\s\w+)*)(?=<\/th>)/)
+  |line| match = (line).scan(/(?<=<(?i:t).>)([\w\s]+)(?=<\/t.>)/i)
   if(match.length > 0)
-   file_csv.puts(match.join(":"))
+   file_csv.puts(match.join(","))
   end
-  match = (line).scan(/(?<=<td>)(?<word>(\w)*(\s\w+)*)(?=<\/td>)/)
-  if(match.length > 0)
-   file_csv.puts(match.join(";"))
-  end  
-}
-
-#Testing the unformatted data
-
-#(<t.>)((<)*t.>\w+(\s\w+)*<\/t.>)(<t.>(\w+)*<\/t.>)(<t.>(\w+)*<\/t.>)(<\/t.>)
-html_content = file_html.readlines
-html_content.each{
-  |line| match = (line).scan(/<t?>/)
-  if(match.length > 0)
-   file_csv_test.puts(match.join(";"))
-  end
-  match = (line).scan(/(?<=<td>)(?<word>(\w)*(\s\w+)*)(?=<\/td>)/)
-  if(match.length > 0)
-   file_csv.puts(match.join(";"))
-  end  
 }
