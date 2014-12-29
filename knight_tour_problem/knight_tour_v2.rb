e293b8b1266b1e3cc9cd6d0edec4b13452863e74
Deepak Kabbur <deepak.kabbur@weboiselab.com>
2014-12-29 09:08:14 +0530

    Knight tour problem version 2

--- knight_tour_problem/knight_tour_v2.rb
+++ knight_tour_problem/knight_tour_v2.rb
@@ -1,10 +1,12 @@
 # usr/bin/ruby -w
 # Datastructure as Graph and Wornsdroff algorithm.
 class Graph
+  Knight_next_move = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], 
+                        [1, 2], [2, -1], [2, 1]]
   def initialize(no_rows, no_cols)
     @board = Array.new(no_rows) { Array.new(no_cols) }
     @path = Array.new(no_rows) { Array.new(no_cols) { '-' } }
-    @count = 0
+    @count = 0 # to count no. moves by knight    
     no_rows.times do |row|
       no_cols.times do |col|
         @board[row][col] = Vertex.new Array.new, row, col
@@ -17,10 +19,7 @@ class Graph
   end
 
   def push_valid_moves(square)
-    knight_next_move = []
-    knight_next_move.push([-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], \
-                          [1, 2], [2, -1], [2, 1])
-    knight_next_move.each do |value|
+    Knight_next_move.each do |value|
       row = square.row + value[0]
       col = square.col + value[1]
       (valid_squre? row, col) && square.edges.push(@board[row][col])
@@ -38,16 +37,19 @@ class Graph
   def start_move(row, col)
     warnsdorf_algorithm @board[row][col]
   end
-
+  # This method used to print the path of knight in matrix form.
   def print_board
+  	puts '___________________________________________'
     puts @count == @board.length**2 ? 'Knight Tour Completed' : 'Incomplete'\
     ' tour of Knight'
+    puts '___________________________________________'
     @path.length.times do |row|
       @path.length.times do |col|
         print "\s #{@path[row][col]} \s"
       end
       puts
     end
+    puts '___________________________________________'
   end
 
   def find_min_moves(next_moves)
@@ -84,5 +86,5 @@ end
 
 obj_graph = Graph.new 5, 5
 obj_graph.generate_moves
-obj_graph.start_move 2, 1
+obj_graph.start_move 0, 0
 obj_graph.print_board
